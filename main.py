54dae09b91f40b254702a3e16e82c8ff7890f762
Allan Burleson <disorientedperson@icloud.com>
2015-12-03 21:59:32 -0600

    New algorithm to find bestinventory weapon, new fight factor--different charachters have varying amounts of strength

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def choosePerson(wantedInfo):
 #    if random.randint(0, 1) == 0:
 #        item = random.choice(peopleHelpers)
 #    else:
-    item = random.choice(peopleWeapons)
+    item = random.choice(weapons)
     if wantedInfo == 'person':
         return person
     elif wantedInfo == 'item':
@@ -20,10 +20,11 @@ def getWeaponPower(item):
 
 
 def getBestInventoryWeapon():
-    bestItem = 0
+    bestItemPower = 0
     for item in inventory:
-        if getWeaponPower(item) > bestItem:
-            bestItem = getWeaponPower(item)
+        if getWeaponPower(item) > bestItemPower:
+            bestItemPower = getWeaponPower(item)
+    return bestItemPower
 
 
 def personInteraction():
@@ -35,7 +36,7 @@ def personInteraction():
         print()
         print('The ' + newPerson + ' pulls out a(n) ' + npi + ' threateningly.')
         time.sleep(1)
-        if getWeaponPower(inventory[0]) > getWeaponPower(npi):
+        if getBestInventoryWeapon() + playerPower > getWeaponPower(npi) + peoplePower[newPerson]:
             print('The ' + newPerson + ' has been defeated!')
         elif getBestInventoryWeapon() == weaponPower[npi]:
             print('Draw!')
@@ -46,12 +47,14 @@ def personInteraction():
         print()
 
 people = ['old lady', 'baby']
-peopleWeapons = ['knife', 'gun', 'cane', 'fist', 'sword']
+peoplePower = {'old lady': 1, 'baby': 1}
+weapons = ['knife', 'gun', 'cane', 'fist', 'sword']
 peopleHelpers = []
 weaponPower = {'stick': 5, 'gun': 50, 'cane': 6, 'fist': 3, 'sword': 40, 'knife': 10}
 inventory = ['stick']
 health = 100
 coins = 100
+playerPower = 5
 
 
 personInteraction()
