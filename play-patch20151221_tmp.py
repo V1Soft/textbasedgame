48050e1395b2d23f755f8720525a7c371633dfc8
ixzaqw2 <connor.haight@gmail.com>
2015-12-21 19:44:55 +0000

    Create play-patch20151221_tmp.py
    Ported "play-patch20151221_tmp" to python3

---
+++ play-patch20151221_tmp.py
@@ -0,0 +1,8 @@
+#!/bin/python3
+import os
+import shelve
+print("Welcome to textbasedgame!\nThis game is released under the GPL.\nCopyright V1Soft 2015")
+shelf = shelve.open('savefile')
+shelf['firstTime'] = True
+shelf.close()
+os.system("python3 main.py")
