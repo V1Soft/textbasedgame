8cc2e8d1a3f04b9119d43bb84d235c3905d3604b
Connor Haight <connor.haight@gmail.com>
2015-12-22 22:28:54 +0000

    Update textbasedgame.py

--- textbasedgame.py
+++ textbasedgame.py
@@ -1,39 +1,7 @@
 #!/usr/bin/python3
 
 import time, random, sys, shelve
-
-class Player(object):
-    def __init__(self, name, health, money, power):
-        self.name = name
-        self.health = health
-        self.money = money
-        self.power = power
-
-    def hit(self, amount):
-        if amount > self.health:
-            return ValueError
-        self.health -= amount
-        return self.health
-
-    def spend(self, amount):
-        if amount > self.money:
-            raise RuntimeError
-        self.money -= amount
-        return self.money
-    
-    def receive(self, amount):
-        self.money += amount
-        return self.money
-
-class Weapon(object):
-    def __init__(self, name, power):
-        self.name = name
-        self.power = power
-
-class Enemy(object):
-    def __init__(self, name,  power):
-        self.name = name
-        self.power = power
+from obj import *
 
 def choosePerson(wantedInfo): # Choose person to interact with
     assert wantedInfo == 'person' or wantedInfo == 'item', 'Bad argument.'
